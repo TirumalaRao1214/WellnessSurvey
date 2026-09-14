e7634d9bba8d34ede635c5188ae08d4efb0ec457
Tirumalarao <Goli.Tirumala.Rao@ibm.com>
2026-09-14 09:23:25 +0530

    Update Google Apps Script deployment URL

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ const CONFIG = {
   contactName: "Moulika",
   phone: "6302552079",
   whatsapp: "916302552079",
-  googleScriptUrl: "https://script.google.com/macros/s/AKfycbzodhxp11d5S2tehrXQ_g9mVELVjLL4ak8NKiYL7xGarYYot6OLeC7t567bwgYPJO6v/exec",
+  googleScriptUrl: "https://script.google.com/macros/s/AKfycbzv92eJseQSe0v_-8K87bhJ7INtjSvwdrTOV0JWXz5JaOgS_vkxkYZ-WvuDSYYwHIGi/exec",
   defaultLanguage: "en",
   surveyDuration: "2–3 minutes",
   langStorageKey: "wellness_lang",
